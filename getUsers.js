b95d09b44e75ee2fd60038ead85e5ae9e977beb7
Kalyani Arikati <kalyani.arikati@gmail.com>
2020-09-21 23:24:25 +1000

    HTML page hide, CSS decoration, setcolor in intervals

--- getUsers.js
+++ getUsers.js
@@ -1,7 +1,7 @@
 import updateHistory from './updateHistory.js';
 import weightChart from './weightChart.js';
 
-
+// get all users to show in users dropdown
     let getUsers=() => {
         axios.get(' http://localhost:3000/users')
             .then(res => {
@@ -11,15 +11,19 @@ import weightChart from './weightChart.js';
                     option.text= data.name;
                     console.log(option);
                     x.add(option);
+                    document.getElementById('page-2').style.visibility = "hidden";
+                    document.getElementById('page-1').style.visibility = "visible";
                 });
                }
                )
             .catch(err => console.err(err))
     }
     
+    // get form id
     let form = document.getElementById('newUser');
     let updateForm = document.getElementById('updateUser');
 
+    // add new user method
     let addUser=()=>{
         let user = {
             name: form.name.value,
@@ -31,11 +35,16 @@ import weightChart from './weightChart.js';
         .catch((err) => console.err(err));
     }
     
+    // call addUser function upon form submit
     form.addEventListener('submit', addUser);
     
     var user={};
 
+    // show weight history, weight chart for an existing user
     document.getElementById('ok').addEventListener('click', ()=>{
+        document.getElementById('newUser').style.visibility = "hidden";
+        document.getElementById('page-2').style.visibility = "visible";
+
     let input = document.getElementById('namesList').value;
         console.log(input)
         axios.get('http://localhost:3000/users')
@@ -50,23 +59,33 @@ import weightChart from './weightChart.js';
     });
 
 
+    // add new weight to the history
     document.getElementById('input-weight').addEventListener('click', (e)=>{
         e.preventDefault();
-        console.log(user);
-        console.log(updateForm.weight.value);
-        console.log(updateForm.inputDate.value);
-
-            let weightLog = {
-                weight: parseInt(updateForm.weight.value),
-                date: updateForm.inputDate.value,
+        // console.log(user);
+        // console.log(updateForm.weight.value);
+        // console.log(updateForm.inputDate.value);
+        if(updateForm.weight.value && updateForm.inputDate.value)
+            {
+                let weightLog = {
+                    weight: parseInt(updateForm.weight.value),
+                    date: updateForm.inputDate.value,
+                }
+                console.log(user[0].weightHistory.push(weightLog));
+                
             }
-            console.log(user[0].weightHistory.push(weightLog));
+            else
+                {
+                    document.getElementById('message').textContent="Enter valid values";
+                   
+                }
             axios.patch(`http://localhost:3000/users/${user[0].id}`, user[0])
-            .then((user) =>{ 
-                updateHistory(user.data);
-                weightChart(user.data.weightHistory);
-            })
-            .catch((err) => console.err(err));
+                .then((user) =>{ 
+                    updateHistory(user.data);
+                    weightChart(user.data.weightHistory);
+                })
+                .catch((err) => console.err(err));
+
         });
 
 
--- input.js
+++ input.js
@@ -1,49 +1,11 @@
 
-let input = [
-    {
-      name: 'Jason',
-      age: 30,
-      weightHistory: [
-        {
-          weight: 65,
-          date: '2020-09-01',
-        },
-        {
-          weight: 67,
-          date: '2020-09-02',
-        },
-        {
-          weight: 69,
-          date: '2020-09-03',
-        },
-        {
-          weight: 63,
-          date: '2020-09-04',
-        },
-      ],
-    },
-    {
-      name: 'Kalyani',
-      age: 32,
-      weightHistory: [
-        {
-          weight: 70,
-          date: '2020-09-05',
-        },
-        {
-          weight: 72,
-          date: '2020-09-06',
-        },
-        {
-          weight: 69,
-          date: '2020-09-07',
-        },
-        {
-          weight: 71,
-          date: '2020-09-08',
-        },
-      ],
-    },
-  ];
 
-  export default input;
+function setColor() {
+  let x = document.body;
+  let y = document.getElementById('page-1');
+  x.style.backgroundColor = x.style.backgroundColor == "lightblue" ? "pink" : 
+  "lightblue";
+  y.style.backgroundColor = y.style.backgroundColor == "pink" ? "lightblue" : "pink";
+}
+
+  export default setColor;
