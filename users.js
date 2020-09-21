199556dbb7b20833019da48360e99aa8187653fe
Kalyani Arikati <kalyani.arikati@gmail.com>
2020-09-21 16:21:31 +1000

    Adduser to RestAPI

--- users.js
+++ users.js
@@ -3,33 +3,34 @@
         axios.get(' http://localhost:3000/users')
             .then(res => {
                 let x = document.getElementById("namesList");
-                
                 res.data.forEach((data) =>  { 
                     let option = document.createElement("option");
                     option.text= data.name;
                     console.log(option);
-                x.add(option);
+                    x.add(option);
                 });
                }
                )
             .catch(err => console.err(err))
     }
-
     getUsers();
-    
-    let user = {
-        name: document.getElementById('name').value,
-          age: document.getElementById('age').value,
-    }
-    console.log(user);
+
+    let form = document.getElementById('newUser');
+   
 
     let addUser=()=>{
+        let user = {
+            name: form.name.value,
+              age: form.age.value,
+              weightHistory:[]
+        }
+        console.log(user);
         axios.post('http://localhost:3000/users', user)
         .then((user) => console.log(user))
         .catch((err) => console.err(err));
     }
     
-    // addUser();
+    form.addEventListener('submit', addUser);
     
     let updateUser=()=>{
         axios.put('http://localhost:3000/users/8', user)
