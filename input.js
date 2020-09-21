46c0a2eecc93f9e0ef0a241dc66b86ecfff24640
Kalyani Arikati <kalyani.arikati@gmail.com>
2020-09-21 10:30:24 +1000

    JSON input file created

---
+++ input.js
@@ -0,0 +1,50 @@
+
+let input = [
+    {
+      name: 'Jason',
+      age: 30,
+      weightHistory: [
+        {
+          weight: 65,
+          date: '1 September 2020',
+        },
+        {
+          weight: 67,
+          date: '2 September 2020',
+        },
+        {
+          weight: 69,
+          date: '3 September 2020',
+        },
+        {
+          weight: 63,
+          date: '8 September 2020',
+        },
+      ],
+    },
+    {
+      name: 'Kalyani',
+      age: 32,
+      weightHistory: [
+        {
+          weight: 70,
+          date: '5 September 2020',
+        },
+        {
+          weight: 72,
+          date: '11 September 2020',
+        },
+        {
+          weight: 69,
+          date: '15 September 2020',
+        },
+        {
+          weight: 71,
+          date: '20 September 2020',
+        },
+      ],
+    },
+  ];
+
+  export default input;
+  
